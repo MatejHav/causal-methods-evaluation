Fix causal forest training and testing

sample/builders.py
@@ -15,8 +15,7 @@ class Experiment:
         self.metrics: Dict[str, Callable[[List[float]], float]] = {}
         self.seed = np.random.seed(seed)
         self.directory = f'experiments/experiment_{seed if seed is not None else f"randomized{self.__hash__()}"}'
-        if not os.path.exists(self.directory):
-            os.mkdir(self.directory)
+        os.makedirs(self.directory, exist_ok=True)
 
     def add_custom_generator(self, generator: Generator, sample_size: int = 500):
         self.generators.append((generator, sample_size))
@@ -44,7 +43,7 @@ class Experiment:
             result = run(model_dictionary, metric_dictionary,
                          data_file=generator.directory + generator.generated_files['data'][-1],
                          samples=sample_size, save_table=save_data,
-                         dir=generator.directory + f'/inter_table_{len(results)}').to_numpy()
+                         dir=generator.directory).to_numpy()
             results = results + result[:, 1:]
 
         results = results / len(self.generators)
@@ -56,13 +55,13 @@ class Experiment:
         columns = list(metric_dictionary.keys())
         columns.insert(0, 'method_name')
         final_result = pd.DataFrame(final_results, columns=columns)
-        save_pandas_table(self.directory + '/table_result', final_result)
+        save_pandas_table(self.directory + '/final_table', final_result)
         return self
 
     # MODELS
 
-    def add_causal_forest(self, number_of_trees=2000, max_leaf_size=1):
-        return self.add_custom_model(CausalForest(number_of_trees, k=max_leaf_size))
+    def add_causal_forest(self, number_of_trees=100, min_leaf_size=10):
+        return self.add_custom_model(CausalForest(number_of_trees, k=min_leaf_size))
 
     def add_dragonnet(self, dimensions):
         return self.add_custom_model(DragonNet(dimensions))
@@ -74,38 +73,70 @@ class Experiment:
                                       lambda truth, pred: np.sum(
                                           [(truth[i] - pred[i]) ** 2 for i in range(len(truth))]) / np.prod(truth.shape))
 
+    def add_absolute_error(self):
+        return self.add_custom_metric('absolute_error',
+                                      lambda truth, pred: np.sum(
+                                          [abs(truth[i] - pred[i]) for i in range(len(truth))]) / np.prod(truth.shape))
+
     # DATA GENERATORS
 
     def add_custom_generated_data(self, main_effect: Callable[[List[float]], float],
                                   treatment_effect: Callable[[List[float]], float],
                                   treatment_propensity: Callable[[List[float]], float],
                                   noise: Callable[[], float], dimensions: int,
-                                  distributions=None):
+                                  distributions=None, sample_size: int = 500):
         if distributions is None:
             distributions = [np.random.random]
         generator = data_generator.Generator(main_effect, treatment_effect, treatment_propensity, noise,
                                              dimensions=dimensions, distributions=distributions)
-        return self.add_custom_generator(generator)
+        return self.add_custom_generator(generator, sample_size=sample_size)
 
-    def add_all_effects_generator(self, dimensions: int):
+    def add_all_effects_generator(self, dimensions: int, sample_size: int = 500):
         main_effect = lambda x: 2 * x[0] - 1
         treatment_effect = lambda x: (1 + 1 / (1 + np.exp(-20 * (x[0] - 1 / 3)))) * (
                 1 + 1 / (1 + np.exp(-20 * (x[1] - 1 / 3))))
         treatment_propensity = lambda x: (1 + beta.pdf(x[0], 2, 4)) / 4
         noise = lambda: 0.05 * np.random.normal(0, 1)
-        return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, dimensions)
+        return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, dimensions,
+                                              sample_size=sample_size)
 
-    def add_no_treatment_effect_generator(self, dimensions: int):
+    def add_no_treatment_effect_generator(self, dimensions: int, sample_size: int = 500):
         main_effect = lambda x: 2 * x[0] - 1
         treatment_effect = lambda x: 0
         treatment_propensity = lambda x: (1 + beta.pdf(x[0], 2, 4)) / 4
         noise = lambda: 0.05 * np.random.normal(0, 1)
-        return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, dimensions)
+        return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, dimensions,
+                                              sample_size=sample_size)
 
-    def add_only_treatment_effect_generator(self, dimensions: int):
+    def add_only_treatment_effect_generator(self, dimensions: int, sample_size: int = 500):
         main_effect = lambda x: 0
         treatment_effect = lambda x: (1 + 1 / (1 + np.exp(-20 * (x[0] - 1 / 3)))) * (
                 1 + 1 / (1 + np.exp(-20 * (x[1] - 1 / 3))))
         treatment_propensity = lambda x: 0.5
         noise = lambda: 0.05 * np.random.normal(0, 1)
-        return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, dimensions)
+        return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, dimensions,
+                                              sample_size=sample_size)
+
+    def add_simple_effects_generator(self, dimensions: int, sample_size: int = 500):
+        main_effect = lambda x: 3 - (2 * x[0] + x[1])
+        treatment_effect = lambda x: 1 - x[0]
+        treatment_propensity = lambda x: x[1]
+        noise = lambda: 0.05 * np.random.normal(0, 1)
+        return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, dimensions,
+                                              sample_size=sample_size)
+
+    def add_small_treatment_propensity_generator(self, dimensions: int, sample_size: int = 500):
+        main_effect = lambda x: 3 - (2 * x[0] + x[1])
+        treatment_effect = lambda x: 1 - x[0]
+        treatment_propensity = lambda x: 0.1 * x[1]
+        noise = lambda: 0.05 * np.random.normal(0, 1)
+        return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, dimensions,
+                                              sample_size=sample_size)
+
+    def add_full_treatment_effect_generator(self, dimensions: int, sample_size: int = 500):
+        main_effect = lambda x: 3 - (2 * x[0] + x[1])
+        treatment_effect = lambda x: 0.5
+        treatment_propensity = lambda x: x[1]
+        noise = lambda: 0.05 * np.random.normal(0, 1)
+        return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, dimensions,
+                                              sample_size=sample_size)

sample/data_generator.py
@@ -35,14 +35,20 @@ class Generator:
         columns.append('main_effect')
         columns.append('treatment_effect')
         columns.append('propensity')
+        columns.append('y0')
+        columns.append('y1')
+        columns.append('noise')
         df = pd.DataFrame([], columns=columns)
         for i in range(number_of_samples):
-            features, treatment, outcome, main_effect, treatment_effect, propensity = self.generate_row()
+            features, treatment, outcome, main_effect, treatment_effect, propensity, y0, y1, noise = self.generate_row()
             features.append(treatment)
             features.append(outcome)
             features.append(main_effect)
             features.append(treatment_effect)
             features.append(propensity)
+            features.append(y0)
+            features.append(y1)
+            features.append(noise)
             df.loc[len(df.index)] = features
         if save_data:
             self.save_data(df)
@@ -52,7 +58,8 @@ class Generator:
                 self.save_graphs()
             if show_graphs:
                 plt.show()
-        return select_features(df, self.dimensions), df['treatment'], df['outcome'], df['main_effect'], df['treatment_effect'], df['propensity']
+        return select_features(df, self.dimensions), df['treatment'], df['outcome'], df['main_effect'], \
+               df['treatment_effect'], df['propensity'], df['y0'], df['y1'], df['noise']
 
     def generate_row(self):
         features = []
@@ -61,11 +68,14 @@ class Generator:
         # W = bernoulli(e(x))
         propensity = self.treatment_propensity(features)
         treatment = 1 if np.random.random() <= propensity else 0
-        treatment_effect = self.treatment_effect(features) # if treatment == 1 else 0
+        treatment_effect = self.treatment_effect(features)
         # Y = m(x) + (W - 0.5) * t(x) + noise
         main_effect = self.main_effect(features)
-        outcome = main_effect + treatment_effect + self.noise()
-        return features, treatment, outcome, main_effect, treatment_effect, propensity
+        noise = self.noise()
+        outcome = main_effect + (treatment - 0.5) * treatment_effect + noise
+        y0 = main_effect - 0.5 * treatment_effect + noise
+        y1 = main_effect + 0.5 * treatment_effect + noise
+        return features, treatment, outcome, main_effect, treatment_effect, propensity, y0, y1, noise
 
     def generate_feature(self, index):
         if len(self.distributions) == 1:
@@ -88,19 +98,19 @@ class Generator:
         plt.scatter(feature_one, feature_two, c=[color_function(i) for i in df.index])
 
     def save_graphs(self):
-        if not os.path.exists(self.directory):
-            os.mkdir(self.directory)
+        os.makedirs(self.directory, exist_ok=True)
         filename = f'/coverage_{time.ctime()}'.replace(' ', '_').replace(':', '-')
         self.generated_files['graphs'].append(filename)
         plt.savefig(self.directory + filename)
 
     def save_data(self, df):
-        if not os.path.exists(self.directory):
-            os.mkdir(self.directory)
+        os.makedirs(self.directory, exist_ok=True)
         filename = f'/generated_data{time.ctime()}.csv'.replace(' ', '_').replace(':', '-')
         self.generated_files['data'].append(filename)
         df.to_csv(self.directory + filename)
 
 
-def select_features(df, dim):
+def select_features(df, dim=-1):
+    if dim == -1:
+        return df[[name for name in df.columns if 'feature' in name]]
     return df[[f'feature_{i}' for i in range(dim)]]

sample/load_data.py
@@ -11,18 +11,6 @@ def load_data_from_generator(generator: data_generator.Generator, samples=500):
 
 def load_data_from_file(csv_file: str):
     df = pd.read_csv(csv_file)
-    dimensions = len(df.columns) - 6
-    return data_generator.select_features(df, dimensions), df['treatment'], df['outcome'], df['main_effect'], df['treatment_effect'], df['propensity']
-
-
-if __name__ == '__main__':
-    main_effect = lambda x: 2 * x[0] - 1
-    treatment_effect = lambda x: 0
-    treatment_propensity = lambda x: (1 + beta.pdf(x[0], 2, 4)) / 4
-    noise = lambda: 0.05 * np.random.normal(0, 1)
-    dimensions = 5
-    distributions = [lambda: np.random.random()]
-    sample_generator = data_generator.Generator(main_effect, treatment_effect, treatment_propensity, noise, dimensions,
-                                                distributions)
-    print(load_data_from_generator(sample_generator))
-    print(load_data_from_file('data/data_dump_81132812397/generated_dataTue_Apr_26_11-20-17_2022.csv'))
+    dimensions = sum([1 for name in df.columns if 'feature' in name])
+    return data_generator.select_features(df, dimensions), df['treatment'], df['outcome'], df['main_effect'],\
+           df['treatment_effect'], df['propensity'], df['y0'], df['y1'], df['noise']