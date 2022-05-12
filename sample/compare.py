ef4fd8a350277bc9fe6beb17f5e4797626b4b5d8
Matej Havelka <M.Havelka@student.tudelft.nl>
2022-05-12 09:47:42 +0200

    Add compare documentation

--- sample/compare.py
+++ sample/compare.py
@@ -1,3 +1,15 @@
+"""Comparator
+
+This file is used to defined functions that runs some models on one set of data and measures some metrics on their
+performances.
+
+This file can also be imported as a module and contains the following
+functions and classes:
+
+    * run: Method that runs input models and measures some metrics. It outputs a dataframe containing the results.
+    * run_model: Method that runs one specific model on the input data and input metrics.
+"""
+
 from causal_effect_methods import *
 from data_generator import *
 from sklearn.model_selection import train_test_split
@@ -8,6 +20,18 @@ def run(methods: Dict[str, CausalMethod],
         score_functions: Dict[str, Callable[[List[float], List[float]], float]],
         data_generator: Generator = None, data_file: str = None, samples: int = 500, save_table: bool = False,
         dir: str = ''):
+    """
+    Method that runs input models and measures some metrics. It outputs a dataframe containing the results.
+    If a data generator is defined, the method will use the generator. Make sure only generator OR data file is defined.
+    :param methods: dictionary of all methods that are to be run
+    :param score_functions: dictionary of all metrics that are to be observed
+    :param data_generator: generator of data that can be used to generate data
+    :param data_file: file from which data will be read from
+    :param samples: number of samples to generate if generator is used
+    :param save_table: boolean representing whether all data should be stored
+    :param dir: directory of where to store data
+    :return: dataframe representing the results observed. Each row is a model where each column is a metric.
+    """
     assert data_generator is not None or data_file is not None, "Data must be either generated or read from a file."
     scoring_list = [score_functions[key] for key in score_functions]
     columns = [key for key in score_functions.keys()]
@@ -39,6 +63,24 @@ def run_model(model: CausalMethod, score_functions: List[Callable[[List[float], 
               treatment_effect: pd.DataFrame, treatment_propensity: pd.DataFrame,
               y0: pd.DataFrame, y1: pd.DataFrame, noise: pd.DataFrame, cate: pd.DataFrame,
               save_table=False, dir=''):
+    """
+    Method that runs one specific model on the input data and input metrics.
+    :param model: Model to test out
+    :param score_functions: list of metrics to observe
+    :param feature_data: dataframe containing all the features
+    :param treatment: dataframe containing the treatments
+    :param outcome: dataframe containing the outcomes
+    :param main_effect: dataframe containing the main_effects
+    :param treatment_effect: dataframe containing the treatment effects
+    :param treatment_propensity: dataframe containing the treatment propensities
+    :param y0: dataframe containing y0's
+    :param y1: dataframe containing y1's
+    :param noise: dataframe containing noise
+    :param cate: dataframe containing cate
+    :param save_table: boolean representing whether table of predictions and the base truth should be stored
+    :param dir: directory where to save table
+    :return: list of observed metrics
+    """
     all_data = feature_data.join(treatment)
     all_data = all_data.join(outcome)
     all_data = all_data.join(main_effect)
