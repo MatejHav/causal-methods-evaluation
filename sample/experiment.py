Add variance graphs and fix default figures

sample/experiment.py
@@ -165,13 +165,13 @@ class Experiment:
     # METRICS
 
     def add_mean_squared_error(self):
-        return self.add_custom_metric('mean_squared_error',
+        return self.add_custom_metric('MSE',
                                       lambda truth, pred: np.sum(
                                           [(truth[i] - pred[i]) ** 2 for i in range(len(truth))]) / np.prod(
                                           truth.shape))
 
     def add_absolute_error(self):
-        return self.add_custom_metric('absolute_error',
+        return self.add_custom_metric('MAE',
                                       lambda truth, pred: np.sum(
                                           [abs(truth[i] - pred[i]) for i in range(len(truth))]) / np.prod(truth.shape))
 
@@ -260,17 +260,24 @@ class Experiment:
                                               sample_size=sample_size, name='biased_generator')
 
     def add_full_biased_generator(self, dimensions: int, sample_size: int = 500):
-        x = lambda : np.random.normal()
+        """
+        X ~ U(0, 1)
+        X -sum(X)-> Y
+        X -sum(X)/len(X)-> W
+        W -Ber(0.05)-> Y
+        Y = 2 * W * W_E
+        CATE = 2 * 1 * 0.05 = 0.1
+        """
         main_effect = lambda x: sum(x)
         treatment_effect = lambda x: 1 if np.random.random() <= 0.05 else 0
-        treatment_propensity = lambda x: 0.5
+        treatment_propensity = lambda x: sum(x) / len(x)
         noise = lambda: np.random.normal(0, 0.01)
         treatment_function = lambda propensity, noise: 1 if np.random.random() <= propensity else 0
         outcome_function = lambda main, treat, treat_eff, noise: 2 * treat * treat_eff + noise
         # E[Y1 - Y0 | X] = E[Y1|X] - E[Y0 | X] = 0.1 - 0 = 0.1
         cate = lambda x: 0.1
         return self.add_custom_generated_data(main_effect, treatment_effect, treatment_propensity, noise, cate,
-                                              dimensions, treatment_function, outcome_function, distributions=[x],
+                                              dimensions, treatment_function, outcome_function,
                                               sample_size=sample_size, name='full_biased_generator')
 
     def add_spiked_generator(self, dimensions: int, sample_size: int = 500):

sample/main.py
@@ -14,8 +14,7 @@ from parameterizer import Parameterizer
 def main():
     t = time.time_ns()
     print('STARTING...')
-    parameterize_sample_size_biased()
-    parameterize_leaf_size()
+    parameterize_number_of_trees()
     print(f'FINISHED IN {(time.time_ns() - t) * 1e-9} SECONDS.')
 
 
@@ -126,10 +125,10 @@ def parameterize_number_of_trees():
         .add_causal_forest(min_leaf_size=1, number_of_trees=d['number_of_trees']) \
         .add_mean_squared_error() \
         .add_full_biased_generator(dimensions=dimensions, sample_size=500)
-    Parameterizer(param_function, tree_numbers, name='number_of_trees_biased_general').run()
+    Parameterizer(param_function, tree_numbers, name='number_of_trees_biased_general').run(epochs=50)
     Parameterizer(param_function, tree_numbers, name='number_of_trees_biased_specific').run_specific(
         pd.DataFrame(np.zeros((40, 5)), columns=[f'feature_{i}' for i in range(dimensions)]),
-        pd.DataFrame(np.zeros((40, 1)) + 0.1, columns=['outcome']))
+        pd.DataFrame(np.zeros((40, 1)) + 0.1, columns=['outcome']), epochs=50)
 
 
 def parameterize_leaf_size():

sample/session.py
@@ -28,6 +28,7 @@ class Session:
 
     def run(self, epochs: int = 10, save_graphs: bool=True):
         results = None
+        variance_results = []
         model_names = None
         metric_names = None
         with alive_bar(epochs) as bar:
@@ -41,14 +42,16 @@ class Session:
                 if results is None:
                     results = np.zeros((len(experiment.models), len(experiment.metrics)))
                 results = results + experiment.results[-1].to_numpy()
+                variance_results.append(experiment.results[-1])
                 bar()
         results = results / epochs
         results = pd.DataFrame(results, columns=metric_names, index=model_names)
         save_pandas_table(self.directory + '/results', results)
-        return results
+        return results, variance_results
 
     def run_specific(self, test_set=pd.DataFrame, truth_set=pd.DataFrame, epochs: int = 10, save_graphs: bool=True):
         results = None
+        variance_results = []
         model_names = None
         metric_names = None
         with alive_bar(epochs) as bar:
@@ -63,9 +66,10 @@ class Session:
                 if results is None:
                     results = np.zeros((len(experiment.models), len(experiment.metrics)))
                 results = results + experiment.results[-1].to_numpy()
+                variance_results.append(experiment.results[-1])
                 bar()
         results = results / epochs
         results = pd.DataFrame(results, columns=metric_names, index=model_names)
         save_pandas_table(self.directory + '/results', results)
-        return results
+        return results, variance_results
 