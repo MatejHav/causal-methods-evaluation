ceb08cd2c3e31297c8da253cb66e7e26b3b991ed
Matej Havelka <M.Havelka@student.tudelft.nl>
2022-05-26 17:09:24 +0200

    Add multithreading

--- sample/session.py
+++ sample/session.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import numpy as np
 import pandas as pd
 
@@ -26,25 +27,45 @@ class Session:
         self.directory = f'sessions/session_{name}/'
         os.makedirs(self.directory, exist_ok=True)
 
+    def batch(self, experiments, results, var_results):
+        for experiment in experiments:
+            experiment.run(save_graphs=False, save_data=True)
+            results.append(experiment.results[-1].to_numpy())
+            var_results.append(experiment.results[-1])
+
     def run(self, epochs: int = 10, save_graphs: bool=True):
-        results = None
+        results_array = []
         variance_results = []
         model_names = None
         metric_names = None
-        with alive_bar(epochs) as bar:
-            for _ in range(epochs):
-                with HiddenPrints():
-                    experiment = self.experiment_function()
-                if model_names is None and metric_names is None:
-                    model_names = [str(model) for model in experiment.models]
-                    metric_names = list(experiment.metrics.keys())
-                experiment.run(save_graphs=False, save_data=True)
-                if results is None:
-                    results = np.zeros((len(experiment.models), len(experiment.metrics)))
-                results = results + experiment.results[-1].to_numpy()
-                variance_results.append(experiment.results[-1])
+        # with alive_bar(epochs) as bar:
+        #     for _ in range(epochs):
+        #         with HiddenPrints():
+        #             experiment = self.experiment_function()
+        #         if model_names is None and metric_names is None:
+        #             model_names = [str(model) for model in experiment.models]
+        #             metric_names = list(experiment.metrics.keys())
+        #         experiment.run(save_graphs=False, save_data=True)
+        #         if results is None:
+        #             results = np.zeros((len(experiment.models), len(experiment.metrics)))
+        #         results = results + experiment.results[-1].to_numpy()
+        #         variance_results.append(experiment.results[-1])
+        #         bar()
+        threads = []
+        # Create 6 threads eah having epoch // 6 experiments
+        number_of_threads = 6
+        for i in range(number_of_threads):
+            experiments = [self.experiment_function() for _ in range(number_of_threads)]
+            if model_names is None and metric_names is None:
+                model_names = [str(model) for model in experiments[0].models]
+                metric_names = list(experiments[0].metrics.keys())
+            threads.append(threading.Thread(target=self.batch, daemon=True, args=(experiments, results_array, variance_results)))
+            threads[-1].start()
+        with alive_bar(number_of_threads) as bar:
+            for i in range(number_of_threads):
+                threads[i].join()
                 bar()
-        results = results / epochs
+        results = sum(results_array) / epochs
         results = pd.DataFrame(results, columns=metric_names, index=model_names)
         save_pandas_table(self.directory + '/results', results)
         return results, variance_results
